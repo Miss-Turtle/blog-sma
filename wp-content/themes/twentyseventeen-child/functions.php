8ee6d232ea48ea6c9378185bcc1dc84e4ddf26de
sandrine <sandrine.martinez1@gmail.com>
2018-04-25 16:53:34 +0200

    Ajout de la librairie prism pour la coloration syntaxique

--- wp-content/themes/twentyseventeen-child/functions.php
+++ wp-content/themes/twentyseventeen-child/functions.php
@@ -1,8 +1,15 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );    
+    wp_register_style('prism-style', get_stylesheet_directory_uri() . '/prism/prism.css');
+    wp_enqueue_style('prism-style');
+}
 
+add_action('wp_enqueue_scripts', 'theme_enqueue_script');
+function theme_enqueue_script(){
+    wp_register_script('prism-js', get_stylesheet_directory_uri() . '/prism/prism.js');
+    wp_enqueue_script('prism-js');
 }
 
 remove_action('wp_head', 'wp_generator');
